4f4a3884a8d39fdf430a0c94452204af5a4cd8cd
kindapyru <lucas.avance.mafort@escola.pr.gov.br>
2026-06-23 11:41:15 -0300

    Add files via upload

---
+++ Aulas/aula 9/atvd 2.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+produtos = [
+    {"id": 1, "nome": "Notebook Dell", "preco": 4500.00, "disponivel": True},
+    {"id": 2, "nome": "Mouse Logitech", "preco": 89.90, "disponivel": True},
+    {"id": 3, "nome": "Teclado Mecânico", "preco": 299.90, "disponivel": False},
+    {"id": 4, "nome": "Monitor 24 polegadas", "preco": 899.90, "disponivel": True}
+]
+
+@app.route('/produtos')
+def listar_produtos():
+    return jsonify(produtos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Aulas/aula 9/atvd 3.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+produtos = [
+    {"id": 1, "nome": "Notebook Dell", "preco": 4500.00, "disponivel": True},
+    {"id": 2, "nome": "Mouse Logitech", "preco": 89.90, "disponivel": True},
+    {"id": 3, "nome": "Teclado Mecânico", "preco": 299.90, "disponivel": False},
+    {"id": 4, "nome": "Monitor 24 polegadas", "preco": 899.90, "disponivel": True}
+]
+
+@app.route('/produtos/<int:id>')
+def buscar_produto(id):
+    for produto in produtos:
+        if produto["id"] == id:
+            return jsonify(produto)
+    
+    return jsonify({"erro": "Produto nao encontrado"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
